2d039d75ae2a14f4162d62ee2294c05759f15f65
Frank Tiersch <mail@ftiersch.de>
2024-02-07 10:45:31 +0000

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,25 @@ sudo chown -R www-data:www-data ./*
 cp ../../deploy/.env .env
 
 # symlink uploads to not lose them
-sudo ln -s "$(pwd)/../../uploads" ./public/storage
-sudo rm -rf ./storage/app/public
-sudo ln -s "$(pwd)/../../uploads" ./storage/app/public
+ln -s "$(pwd)/../../uploads" ./public/storage
+rm -rf ./storage/app/public
+ln -s "$(pwd)/../../uploads" ./storage/app/public
 
 # symlink logs to not lose them
-sudo rm -rf ./storage/logs
-sudo ln -s "$(pwd)/../../logs" ./storage/logs
+rm -rf ./storage/logs
+ln -s "$(pwd)/../../logs" ./storage/logs
 
 # change ownership for symlinks
-sudo chown -h www-data:www-data ./public/storage
-sudo chown -h www-data:www-data ./storage/app/public
-sudo chown -h www-data:www-data ./storage/logs
+chown -h www-data:www-data ./public/storage
+chown -h www-data:www-data ./storage/app/public
+chown -h www-data:www-data ./storage/logs
 
-sudo rm ../../current
-sudo ln -s "$(pwd)/public" ../../current
-sudo chown -h www-data:www-data ../../current
+rm ../../current
+ln -s "$(pwd)/public" ../../current
+chown -h www-data:www-data ../../current
 
 php artisan migrate --force
 
 # delete old releases
 cd ..
-sudo sh -c 'ls -1t | tail +3 | xargs rm -rf'
+sh -c "ls -1t | tail +3 | xargs rm -rf"
